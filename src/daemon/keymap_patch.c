e69588c32e2075d07e0833bb840aaa256ad4caf2
Gil Vicente <gilvbp@gmail.com>
2025-09-19 12:08:43 -0300

    remove redundant K70_CORE_RGB patches from keymap

--- src/daemon/keymap_patch.c
+++ src/daemon/keymap_patch.c
@@ -139,9 +139,7 @@ static const keypatches mappatches[] = {
     ADD_PATCH(V_CORSAIR, P_K70_TKL_CHAMP_OPTIC, k70tklpatch),
     ADD_PATCH(V_CORSAIR, P_K70_PRO,       k70propatch),
     ADD_PATCH(V_CORSAIR, P_K70_PRO_OPTIC, k70propatch),
-    ADD_PATCH(V_CORSAIR, P_K70_CORE_RGB, k70corergbpatch),
-    ADD_PATCH(V_CORSAIR, P_K70_CORE_RGB_2, k70corergbpatch),
-    ADD_PATCH(V_CORSAIR, P_K70_CORE_RGB_3, k70corergbpatch),
+
 };
 
 #define KEYPATCHES_LEN sizeof(mappatches)/sizeof(*mappatches)
